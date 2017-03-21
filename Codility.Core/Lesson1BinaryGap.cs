d1cc7223d2d7001976ef14416e25a840f5f4ef07
Michael Hook <michael.hook@palmerharvey.co.uk>
2017-03-21 10:09:15 +0000

    Solution for Lesson 1 - BinaryGap.

---
+++ Codility.Core/Lesson1BinaryGap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Core
+{
+    public class Lesson1BinaryGap
+    {
+        public static int Solution(int N)
+        {
+            var NString = Convert.ToString(N, 2);
+            var NArray = NString.ToCharArray();
+
+            if (NString.Contains("0") == false || NString.Contains("1") == false)
+            {
+                return 0;
+            }
+
+            if (NArray.Count(c => c == '1') < 2)
+            {
+                return 0;
+            }
+
+            var oneIndexes = new List<int>();
+            for (int i = 0; i < NArray.Length; i++)
+            {
+                if (NArray[i] == '1')
+                {
+                    oneIndexes.Add(i);
+                }
+            }
+
+            var gaps = new List<int>();
+            for (int i = oneIndexes.Count - 1; i >= 1; --i)
+            {
+                gaps.Add(oneIndexes[i] - oneIndexes[i - 1]);
+            }
+
+            return gaps.OrderBy(x => x).Last() - 1;
+        }
+    }
+}
---
+++ Codility.Tests/Lesson1BinaryGapTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Codility.Tests
+{
+    using System.Collections;
+    using Core;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Lesson1BinaryGapTests
+    {
+
+        [Test, TestCaseSource(typeof(MyDataClass), "TestCases")]
+        public int Test(int n)
+        {
+            return Lesson1BinaryGap.Solution(n);
+        }
+
+        public class MyDataClass
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    yield return new TestCaseData(9).Returns(2);
+                    yield return new TestCaseData(529).Returns(4);
+                    yield return new TestCaseData(20).Returns(1);
+                    yield return new TestCaseData(15).Returns(0);
+                    yield return new TestCaseData(2147483647).Returns(0);
+                }
+            }
+        }
+
+    }
+}
