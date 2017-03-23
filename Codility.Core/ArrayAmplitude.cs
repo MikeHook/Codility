69f1d73a7b99e2f9cd73e7dc422a1eea1451a6aa
Michael Hook <michael.hook@palmerharvey.co.uk>
2017-03-23 09:21:54 +0000

    Added ArrayAmplitude tests.

---
+++ Codility.Core/ArrayAmplitude.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Codility.Core
+{
+    public class ArrayAmplitude
+    {
+        //The amplitude of a non-empty zero-indexed array A consisting of N numbers is defined as 
+        //amplitude(A) = max { A[P] - A[Q] : 0 <= P, Q < N } 
+
+        public static int Solution(int[] A)
+        {
+            if (A == null || A.Length == 0)
+            {
+                return 0;
+            }
+
+            if (A.Length == 1)
+            {
+                return A[0];
+            }
+
+            var orderedA = A.OrderBy(a => a);
+
+            var result = (long) orderedA.Last() - (long) orderedA.First();
+            return result > int.MaxValue ? 0 : (int) result;
+        }
+    }
+}
---
+++ Codility.Tests/ArrayAmplitudeTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Codility.Tests
+{
+    using System.Collections;
+    using Core;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ArrayAmplitudeTests
+    {
+
+        [Test, TestCaseSource(typeof(MyDataClass), "TestCases")]
+        public int Test(int[] a)
+        {
+            return ArrayAmplitude.Solution(a);
+        }
+
+        public class MyDataClass
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    yield return new TestCaseData(new int[] { 9,0}).Returns(9);
+                    yield return new TestCaseData(new int[] { 2147483647, 0}).Returns(2147483647);
+                    yield return new TestCaseData(new int[] { 2147483647, -1}).Returns(0);  //Unknown what this answer should be
+                    yield return new TestCaseData(new int[] { 1}).Returns(1);
+                    yield return new TestCaseData(new int[] { 1, 1}).Returns(0);
+                    yield return new TestCaseData(new int[] { }).Returns(0);
+                }
+            }
+        }
+
+    }
+}
